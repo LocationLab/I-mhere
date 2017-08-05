373d4bde25343e77b6ea77dc93c81889709c6016
Andrey <wantedmins@gmail.com>
2017-08-05 08:49:37 +0300

    add
    add

---
+++ app/src/main/java/com/asergeev/imhere/activity/Children_Secure.java
@@ -0,0 +1,26 @@
+package com.asergeev.imhere.activity;
+
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.asergeev.imhere.R;
+
+/**
+ * Created by Andrey on 04.08.2017.
+ */
+
+public class Children_Secure extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secure);
+
+
+    }
+
+}
